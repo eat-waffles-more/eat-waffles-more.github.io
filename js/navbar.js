d94e98aa8d7f4cf6f89a54aadefafb533f1a5dbb
Waffles Account <olsenree002@alpinesd.org>
2025-04-22 17:51:08 -0600

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -14,11 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
         <div class="nav-links">
           <a href="/home"><i class="fa fa-home fa-lg"></i></a>
         <a href="/games"><i class="fa fa-gamepad fa-lg"></i></a>
-        <a href="/music"><i class="fa fa-music fa-lg"></i></a>
         <a href="/theater"><i class="fa fa-tv fa-lg"></i></a>
         <a href="/forms"><i class="fa fa-clipboard-list fa-lg"></i></a>
         <a href="/reviews"><i class="fa fa-star fa-lg"></i></a>
         <a href="/contact" class="margin"><i class="fa fa-user fa-lg"></i></a>
+        <a href="/contact-us"><i class="fa-solid fa-envelope"></i></a>
       </div>
       </div>
       <!-- Right section: Profile information -->
