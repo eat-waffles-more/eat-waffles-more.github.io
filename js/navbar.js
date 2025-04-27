b92c63a9f7815a2bec3f30628b6ba16c8dfbb01b
Waffles Account <olsenree002@alpinesd.org>
2025-04-27 15:41:33 -0600

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -17,9 +17,14 @@ document.addEventListener('DOMContentLoaded', () => {
         <a href="/theater"><i class="fa fa-tv fa-lg"></i></a>
         <a href="/forms"><i class="fa fa-clipboard-list fa-lg"></i></a>
         <a href="/reviews"><i class="fa fa-star fa-lg"></i></a>
-        <a href="/contact" class="margin"><i class="fa fa-user fa-lg"></i></a>
-        <a href="/contact"><i class="fa-solid fa-envelope"></i></a>
+        <a href="/contact"><i class="fa fa-user fa-lg"></i></a>
+        <a href="javascript:void(0);" class="extra"><i class="fa fa-plus fa-lg"></i></a>
+        <div class="extra-buttons" style="display: none;">
         <a target="blank" href="https://github.com/eat-waffles-more"><i class="fa-brands fa-github fa-lg"></i></a>
+        <a href="/terms"><i class="fa-solid fa-clipboard-check"></i></a>
+        <a href="/privacy"><i class="fa-solid fa-user-lock"></i></a>
+        <a href="/contact"><i class="fa-solid fa-envelope"></i></a>
+        </div>
       </div>
       </div>
       <!-- Right section: Profile information -->
@@ -34,4 +39,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Inject the navbar at the very top of the body
   document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+  const extraIcon = document.querySelector('.extra');
+  const extraButtons = document.querySelector('.extra-buttons');
+
+  plusIcon.addEventListener('click', () => {
+    if (extraLinks.style.display === 'none') {
+      extraButtons.style.display = 'flex';
+      plusIcon.innerHTML = '<i class="fa fa-minus fa-lg"></i>';
+    } else {
+      extraButtons.style.display = 'none';
+      plusIcon.innerHTML = '<i class="fa fa-plus fa-lg"></i>';
+    }
+  });
+});
 });
